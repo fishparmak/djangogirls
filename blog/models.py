b8d581aa45ed3443064089c5f5708ed58739c36b
Banana <dana140198@gmail.com>
2018-10-30 12:34:07 +0600

    add db logic
    models relation

--- blog/models.py
+++ blog/models.py
@@ -16,31 +16,51 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title        return self.title
+        return self.title
+
+
+
+
 
 class Movie(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=200, default='Movie')
+    description = models.TextField(default='This is Movie description')
 
     def __str__(self):
         return self.title
 
 class Cinema(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=200, default='Cinema')
+    description = models.TextField(default='This is Cinema description')
 
     def __str__(self):
         return self.title
 
 class Hall(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='Hall')
     cinema = models.ForeignKey('Cinema', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Seat(models.Model):
-    sold = models.BooleanField()
+    sold = models.BooleanField(default='False')
     hall = models.ForeignKey('Hall', on_delete=models.CASCADE)
     def __str__(self):
         return self.id
+
+class MovieHall(models.Model):
+    time = models.TimeField(default= timezone.now())
+    hall = models.ForeignKey('Hall', on_delete=models.CASCADE)
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Ticket(models.Model):
+    cost = models.DecimalField(default=5.99, max_digits = 6, decimal_places = 2)
+    movieHall = models.ForeignKey('MovieHall', on_delete=models.CASCADE)
+    seat = models.ForeignKey('Seat', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
