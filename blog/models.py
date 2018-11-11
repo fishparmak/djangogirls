ba900b412f6272e77e0d457e5d52519e64ca5cf5
Banana <dana140198@gmail.com>
2018-11-11 08:32:50 +0600

    fix max

--- blog/models.py
+++ blog/models.py
@@ -53,10 +53,10 @@ class User(models.Model):
 
 
 class Team(models.Model):
-    max = models.IntegerField(default=5, null = True, blank = True)
     name = models.CharField(max_length=200,null=True, blank=True, default='Name')
     description = models.TextField( null=True, blank=True, default='Description')
     created_date = models.DateField(default=datetime.date.today)
+    max = models.IntegerField(default=5, null = True, blank = True)
 
     def __str__(self):
         return self.name
